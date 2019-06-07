4a6c210d56f0023649c57ffab7033b2ec7f4944f
abhijeetkhire@gmail.com <abhijeetkhire@gmail.com>
2019-06-07 22:18:39 +0530

    Revert "all basics"
    This reverts commit 532c387548786464be94dbd00771f1a07d5a0794.

--- src/actions/authActions.js
+++
@@ -1,5 +0,0 @@
-import { GET_ADMIN_NAME } from "../actions/types";
-
-export const getAdminName = adminName => {
-  return { type: GET_ADMIN_NAME, adminName };
-};
--- src/actions/types.js
+++
@@ -1,4 +0,0 @@
-export const GET_STUDENTS = "GET_STUDENTS";
-
-// admin
-export const GET_ADMIN_NAME = "GET_ADMIN_NAME";
